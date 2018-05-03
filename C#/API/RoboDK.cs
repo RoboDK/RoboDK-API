cf496fbe49720ec0ed47727f7fcf04bcb2f2d18c
Noel Rimensberger <noel.rimensberger@zuehlke.com>
2018-05-03 14:32:24 +0200

    fix timeout for CloseStation

--- C#/API/RoboDK.cs
+++ C#/API/RoboDK.cs
@@ -744,7 +744,9 @@ namespace RoboDk.API
             var command = "Remove";
             send_line(command);
             send_item(new Item(this));
+            ReceiveTimeout = 3600 * 1000;
             check_status();
+            ReceiveTimeout = DefaultSocketTimeoutMilliseconds;
         }
 
         /// <inheritdoc />
