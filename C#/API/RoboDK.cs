f062fb6752e9a80c1756a33dea8ca8f45969a0e0
RoboDK <RoboDK@users.noreply.github.com>
2018-08-15 09:54:09 -0400

    Fixed SetInteractiveMode Flag type

--- C#/API/RoboDK.cs
+++ C#/API/RoboDK.cs
@@ -1586,7 +1586,7 @@ namespace RoboDk.API
         }
 
         /// <inheritdoc />
-        public void SetInteractiveMode(InteractiveType modeType = InteractiveType.MoveReferences, DisplayRefType defaultRefFlags = DisplayRefType.DEFAULT, List<IItem> customItems = null, List<InteractiveType> customRefFlags = null)
+        public void SetInteractiveMode(InteractiveType modeType = InteractiveType.MoveReferences, DisplayRefType defaultRefFlags = DisplayRefType.DEFAULT, List<IItem> customItems = null, List<DisplayRefType> customRefFlags = null)
         {
             check_connection();
             send_line("S_InteractiveMode");
