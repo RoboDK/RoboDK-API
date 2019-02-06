77e46cae5dea83bbfa603ea9ad8674cd2625220b
noelpenne <noel.rimensberger@bluewin.ch>
2019-02-06 10:56:09 +0100

    refactor collisionItem and collisionPair

--- C#/API/CollisionPair.cs
+++ C#/API/CollisionPair.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoboDk.API
+﻿namespace RoboDk.API
 {
     public class CollisionPair
     {
-        public IItem item1;
-        public IItem item2;
-        public int id1;
-        public int id2;
+        public CollisionPair(IItem item1, int id1, IItem item2, int id2)
+        {
+            CollisionItem1 = new CollisionItem(item1, id1);
+            CollisionItem2 = new CollisionItem(item2, id2);
+        }
+
+        public CollisionPair(IItem item1, IItem item2)
+        {
+            CollisionItem1 = new CollisionItem(item1);
+            CollisionItem2 = new CollisionItem(item2);
+        }
+
+        public CollisionItem CollisionItem1 { get; }
+        public CollisionItem CollisionItem2 { get; }
+
+        public IItem Item1 => CollisionItem1.Item;
+
+        public IItem Item2 => CollisionItem2.Item;
+
+        public int RobotLinkId1 => CollisionItem1.RobotLinkId;
+
+        public int RobotLinkId2 => CollisionItem2.RobotLinkId;
     }
 }
