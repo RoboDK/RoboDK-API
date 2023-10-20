087669278ba4e5b08229f7c393b631f56422ed89
Sam <samuel@robodk.com>
2023-10-20 08:41:21 -0400

    Revert commited files for Python

--- Python/Examples/Scripts/ScaleXYZ.py
+++ Python/Examples/Scripts/ScaleXYZ.py
@@ -7,6 +7,38 @@ from robodk.robolink import *  # RoboDK API
 from robodk.robomath import *  # Robot toolbox
 from robodk.robodialogs import *
 
+
+def str2floatlist(str_values, expected_values):
+    """Converts a string to a list of values. It returns None if the array is smaller than the expected size."""
+    import re
+    if str_values is None:
+        return None
+    values = re.findall("[-+]?\d+[\.]?\d*", str_values)
+    if len(values) < expected_values:
+        return None
+    for i in range(len(values)):
+        values[i] = float(values[i])
+    #print('Read values: ' + repr(values))
+    return values
+
+
+def mbox_getfloatlist(title_msg, show_value, expected_values):
+    """Get a list of values from the user, stops the script if the user hits cancel"""
+    if type(show_value) == Mat:
+        show_value = show_value.tolist()
+    answer = mbox(title_msg, entry=str(show_value))
+    if answer is False:
+        print('Operation cancelled by user')
+        quit(0)
+        #raise Exception('Operation cancelled by user')
+
+    #print('Input value: ' + answer)
+    values = str2floatlist(answer, expected_values)
+    #if len(values) < expected_values:
+    #    raise Exception('%i values expected' % expected_values)
+    return values
+
+
 # Connect to RoboDK API
 RDK = Robolink()
 
@@ -14,12 +46,11 @@ RDK = Robolink()
 RDK.setSelection([])
 
 # Ask the user to select an object or tool
-obj = RDK.ItemUserPick('Select an object or tool to scale', RDK.ItemList(ITEM_TYPE_TOOL) + RDK.ItemList(ITEM_TYPE_OBJECT))
+obj = RDK.ItemUserPick('Select an object or tool to scale')
 if not obj.Valid():
     quit(0)
 
-# Retrieve the scaling factors
-scale_xyz = InputDialog("Enter a per-axis scale", [1.0, 1.0, 1.0])
+scale_xyz = mbox_getfloatlist("Enter a per-axis scale", [1, 1, 1], 3)
 if not scale_xyz:
     quit(0)
 
