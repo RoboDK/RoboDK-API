07b31896bd1a5941b1489be0b8b91313e61eb049
raphael-robodk <raphael@robodk.com>
2025-04-01 13:08:21 -0400

    Reformated styling

--- Python/Examples/Scripts/Camera_FoV_Verification.py
+++ Python/Examples/Scripts/Camera_FoV_Verification.py
@@ -5,10 +5,8 @@
 #
 # This example shows how to create a RoboDK Shape to represent a camera's FoV
 # and verify if an object is within that shape.
-from robodk.robolink import *
+from robodk.robolink import Robolink, ITEM_TYPE_CAMERA, ITEM_TYPE_OBJECT
 from robodk import robolink, robomath, robodialogs, roboapps, robolinkutils
-
-
 
 #----------------------------------
 # You might need to play arround these settings depending on the object/setup
@@ -24,6 +22,7 @@ if not cam_item.Valid():
     cam_item = RDK.Cam2D_Add(RDK.ActiveStation())  # Do not set the DEPTH option here, as we are retrieving both RGB and DEPTH images
     cam_item.setName(CAMERA_NAME)
 cam_item.setParam('Open', 1)
+
 
 #----------------------------------
 # Retrieve camera settings / camera matrix
@@ -59,28 +58,26 @@ pixel_size = cam_settings['PIXELSIZE']
 camera_range = cam_settings['FAR_LENGTH']
 
 # Creates cam FoV Shape using cam settings
-HFOV = 2*robomath.atan2((w * pixel_size/1000.0) , (2 * focal_length))    
-VFOV = 2*robomath.atan2((h * pixel_size/1000.0) , (2 * focal_length))
-FoV_Items = []
-FoV_Items.append(RDK.AddShape([[0,0,0],[camera_range * robomath.tan(HFOV/2), camera_range * robomath.tan(VFOV/2), camera_range],[-camera_range * robomath.tan(HFOV/2), camera_range * robomath.tan(VFOV/2), camera_range]]))
-FoV_Items.append(RDK.AddShape([[0,0,0],[-camera_range * robomath.tan(HFOV/2), camera_range * robomath.tan(VFOV/2), camera_range],[-camera_range * robomath.tan(HFOV/2), -camera_range * robomath.tan(VFOV/2), camera_range]]))
-FoV_Items.append(RDK.AddShape([[0,0,0],[camera_range * robomath.tan(HFOV/2), -camera_range * robomath.tan(VFOV/2), camera_range],[camera_range * robomath.tan(HFOV/2), camera_range * robomath.tan(VFOV/2), camera_range]]))
-FoV_Items.append(RDK.AddShape([[0,0,0],[-camera_range * robomath.tan(HFOV/2), -camera_range * robomath.tan(VFOV/2), camera_range],[camera_range * robomath.tan(HFOV/2), -camera_range * robomath.tan(VFOV/2), camera_range]]))
-FoV_Items.append(RDK.AddShape([[-camera_range * robomath.tan(HFOV/2), -camera_range * robomath.tan(VFOV/2), camera_range],[camera_range * robomath.tan(HFOV/2), -camera_range * robomath.tan(VFOV/2), camera_range],[-camera_range * robomath.tan(HFOV/2), camera_range * robomath.tan(VFOV/2), camera_range]]))
-FoV_Items.append(RDK.AddShape([[camera_range * robomath.tan(HFOV/2), camera_range * robomath.tan(VFOV/2), camera_range],[camera_range * robomath.tan(HFOV/2), -camera_range * robomath.tan(VFOV/2), camera_range],[-camera_range * robomath.tan(HFOV/2), camera_range * robomath.tan(VFOV/2), camera_range]]))
+HFOV = 2 * robomath.atan2((w * pixel_size / 1000.0), (2 * focal_length))
+VFOV = 2 * robomath.atan2((h * pixel_size / 1000.0), (2 * focal_length))
+fov_items = []
+fov_items.append(RDK.AddShape([[0, 0, 0], [camera_range * robomath.tan(HFOV / 2), camera_range * robomath.tan(VFOV / 2), camera_range], [-camera_range * robomath.tan(HFOV / 2), camera_range * robomath.tan(VFOV / 2), camera_range]]))
+fov_items.append(RDK.AddShape([[0, 0, 0], [-camera_range * robomath.tan(HFOV / 2), camera_range * robomath.tan(VFOV / 2), camera_range], [-camera_range * robomath.tan(HFOV / 2), -camera_range * robomath.tan(VFOV / 2), camera_range]]))
+fov_items.append(RDK.AddShape([[0, 0, 0], [camera_range * robomath.tan(HFOV / 2), -camera_range * robomath.tan(VFOV / 2), camera_range], [camera_range * robomath.tan(HFOV / 2), camera_range * robomath.tan(VFOV / 2), camera_range]]))
+fov_items.append(RDK.AddShape([[0, 0, 0], [-camera_range * robomath.tan(HFOV / 2), -camera_range * robomath.tan(VFOV / 2), camera_range], [camera_range * robomath.tan(HFOV / 2), -camera_range * robomath.tan(VFOV / 2), camera_range]]))
+fov_items.append(RDK.AddShape([[-camera_range * robomath.tan(HFOV / 2), -camera_range * robomath.tan(VFOV / 2), camera_range], [camera_range * robomath.tan(HFOV / 2), -camera_range * robomath.tan(VFOV / 2), camera_range], [-camera_range * robomath.tan(HFOV / 2), camera_range * robomath.tan(VFOV / 2), camera_range]]))
+fov_items.append(RDK.AddShape([[camera_range * robomath.tan(HFOV / 2), camera_range * robomath.tan(VFOV / 2), camera_range], [camera_range * robomath.tan(HFOV / 2), -camera_range * robomath.tan(VFOV / 2), camera_range], [-camera_range * robomath.tan(HFOV / 2), camera_range * robomath.tan(VFOV / 2), camera_range]]))
 
 # Merges all created items into 1 shape
-FoV = RDK.MergeItems(FoV_Items)
-FoV.setName("Camera FoV")
-FoV.setParent(cam_item)
-FoV.setVisible(False)
-
+fov = RDK.MergeItems(fov_items)
+fov.setName("Camera FoV")
+fov.setParent(cam_item)
+fov.setVisible(False)
 
 # Select an existing object and verifies if the object is seen by the camera
 obj_item = RDK.ItemUserPick(itemtype_or_list=ITEM_TYPE_OBJECT)
 if not obj_item.Valid():
     obj_item = None
-    
-if obj_item is not None:
-    obj_item.IsInside(FoV)
 
+if obj_item is not None:
+    obj_item.IsInside(fov)
